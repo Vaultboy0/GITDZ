1d51c98fb6f1ba817c7e185a80f4c20e14041f2e
Vault_boy0 <oleg_95.95@mail.ru>
2023-02-25 15:44:32 +0300

    Второй коммит

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 
 string[] FindLessThan(string[] input, int n) {
-    string[] output = new string[CountLessThan(input, n)];
+    string[] output = new string[CountLess(input, n)];
 
     for(int i = 0, j = 0; i < input.Length; i++) {
         if(input[i].Length <= n) {
@@ -10,7 +10,17 @@ string[] FindLessThan(string[] input, int n) {
     }
     return output;
 }
+int CountLess(string[] input, int n) {
+    int count = 0;
 
+    for(int i = 0; i < input.Length; i++) {
+        if(input[i].Length <= n) {
+            count++;
+        }
+    }
+
+    return count;
+}
 
 
 string[] array = AskArray();
